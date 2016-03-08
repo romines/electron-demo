77047d1fb27d54259c0e2c96ffb301b4e7e5cc5a
Adam Romines <adam.romines@gmail.com>
2016-03-08 12:17:57 -0700

    Hello world and local file example

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict';
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+var mainWindow = null;
+
+app.on('ready', function () {
+
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+})
