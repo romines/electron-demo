b242bfb754783a2fb0f93d90402fe4688247f83f
Adam Romines <adamromines@gmail.com>
2016-03-08 17:59:34 -0700

    Finished menubar app

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 'use strict';
 const electron = require('electron');
 const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-
-var mainWindow = null;
-
-app.on('ready', function () {
-
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600
-  });
-
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-
-});
-
-// const menubar = require('menubar');
+// const BrowserWindow = electron.BrowserWindow;
 //
-// var mb = menubar();
+// var mainWindow = null;
+//
+// app.on('ready', function () {
+//
+//   mainWindow = new BrowserWindow({
+//     width: 800,
+//     height: 600
+//   });
+//
+//   mainWindow.loadURL('file://' + __dirname + '/index.html');
+//
+// });
+
+const menubar = require('menubar');
+
+var mb = menubar();
